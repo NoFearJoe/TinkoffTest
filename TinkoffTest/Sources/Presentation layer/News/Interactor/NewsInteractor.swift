1051084173373db33cf6807381cfb65e78273fbd
IlyaKharabet <ilyakharabet@gmail.com>
2017-07-13 14:02:16 +0300

    Add request manager

---
+++ TinkoffTest/Sources/Presentation layer/News/Interactor/NewsInteractor.swift
@@ -0,0 +1,17 @@
+//
+//  NewsInteractor.swift
+//  TinkoffTest
+//
+//  Created by Ilya Kharabet on 13.07.17.
+//  Copyright © 2017 IK. All rights reserved.
+//
+
+
+
+final class NewsInteractor {
+
+    func obtainNews() {
+        
+    }
+
+}
